5cf70f7b18898961ce94415e98157f0634d686f9
Your Name <s.r.j.s.n2017@gmail.com>
2024-11-13 22:57:00 +0530

    categories added

--- src/pages/user/HomePage.jsx
+++ src/pages/user/HomePage.jsx
@@ -1,9 +1,10 @@
+import Categories from "../../components/categories/Categories"
 import UserLayout from "../../shared/UserLayout"
 
 const HomePage = () => {
     return (
         <UserLayout>
-        
+            <Categories />
         </UserLayout>
     )
 }
